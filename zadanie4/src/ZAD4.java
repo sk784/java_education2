fff82cde8cbc08def1b7108f1a637010995328f0
sk784 <sk784@yandex.ru>
2018-12-03 18:28:40 +0300

    сделал задание 4

---
+++ zadanie4/src/ZAD4.java
@@ -0,0 +1,71 @@
+public class ZAD4 {
+    static final Object obj = new Object();
+    static volatile char ch = 'A';
+
+    public static void main(String[] args) {
+        new Thread(new A()).start();
+        new Thread(new B()).start();
+        new Thread(new C()).start();
+    }
+
+
+    static class A implements Runnable {
+        @Override
+        public void run() {
+            synchronized (obj) {
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        while (ch != 'A')
+                            obj.wait();
+                        System.out.print("A");
+                        ch = 'B';
+                        obj.notifyAll();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class B implements Runnable {
+        @Override
+        public void run() {
+            synchronized (obj) {
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        while (ch != 'B')
+                            obj.wait();
+                        System.out.print("B");
+                        ch = 'C';
+                        obj.notifyAll();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class C implements Runnable {
+        @Override
+        public void run() {
+            synchronized (obj) {
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        while (ch != 'C')
+                            obj.wait();
+                        System.out.print("C");
+                        ch = 'A';
+                        obj.notifyAll();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
+
+
